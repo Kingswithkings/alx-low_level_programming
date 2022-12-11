c07a8535b4685acbbb1543b106b8e4ddb21e633e
kingswithkings <kingsuthanaidogu>
2022-12-11 14:51:08 -0800

    ladydora

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- *  main - Entry Point
+ * main - print a line of code using puts
  *
- *  Return: Always 0 (Success)
+ * Return: 0
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
