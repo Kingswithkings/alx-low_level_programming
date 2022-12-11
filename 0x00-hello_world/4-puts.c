37e1658937fc2e4d200ff163f33b99818ce75803
kingswithkings <kingsuthanaidogu>
2022-12-11 03:25:19 -0800

    love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
-<<<<<<< HEAD
- * main - main block
- *Return: o
- */
-int main(void)
-{
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
-=======
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle"
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
  *   * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
+
 int main(void)
+
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
->>>>>>> 7e17d5cd44ed416934ba9b950cb4cfb8a9b3959f
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
+
+
