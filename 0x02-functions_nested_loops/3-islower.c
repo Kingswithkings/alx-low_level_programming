30e894068d0a615497e5b6aa0c9c9900de2ae4c4
kingswithkings <kingsuthanaidogu>
2022-12-13 15:56:09 -0800

    kings

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,11 @@
  * letter from the English alphabet
  * @c: input parameter
  *Return: 1 for lowercase, 0 if it's not lowercase
- /
- int _islower(int c)
- {
- if (c >= 'a' && c <= 'z')
- return (1);
- else
- return (0);
- }
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
