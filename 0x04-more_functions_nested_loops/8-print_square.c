e3d985d52fc4237c276bb954bafc40fe531ce111
kingswithkings <kingsuthanaidogu>
2022-12-16 21:07:27 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,16 @@
 void print_square(int size)
 {
 int co, ro;
-if (size <= 0)
+	if (size <= 0)
 {
 _putchar('\n');
 }
-else
+	else
 {
-for (co = 1; co <= size; co++)
+	for (co = 1; co <= size; co++)
 {
 _putchar('#');
-for (ro = 2; ro <= size; ro++)
+	for (ro = 2; ro <= size; ro++)
 {
 }
 
